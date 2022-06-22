5941e9f2581683b8486e3b189b60d15a6528b9e9
Girish Raja <graja@vmware.com>
2022-06-22 17:41:57 +0530

    MavenBuild

---
+++ maven-build-demo/src/main/java/com/example/mavenbuilddemo/MavenBuildDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenbuilddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenBuildDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenBuildDemoApplication.class, args);
+	}
+
+}
